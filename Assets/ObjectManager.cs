32333a7ee4b60cb8cf1a8b87fa56f7d822fea5a2
petpetshoy <kenithjones@hotmail.ca>
2020-03-10 20:42:12 -0700

    added other page views
    Added in the other five page views, still need to uniformly seperate them and add in code for shifting between them.

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -7,19 +7,35 @@ public class ObjectManager : MonoBehaviour
 {
 
     public GameObject ModalPanelBackdrop;
+    public GameObject ModalStatsPanel;
+    public GameObject ModalACPanel;
+    public GameObject ModalResistancesPanel;
+    public GameObject ModalHPPanel;
+    public GameObject ModalNamePanel;
 
     public Button StatsButton;
-    public Button SavesButton;
+    public Button ResistancesButton;
+    public Button ACButton;
+    public Button HPButton;
+    public Button NameButton;
 
     // Start is called before the first frame update
     void Start()
     {
         // Initialize the main Views
         ModalPanelBackdrop = GameObject.Find("ModalPanelBackdrop");
+        ModalStatsPanel = GameObject.Find("ModalStatsPanel");
+        ModalACPanel = GameObject.Find("ModalACPanel");
+        ModalResistancesPanel = GameObject.Find("ModalResistancesPanel");
+        ModalHPPanel = GameObject.Find("ModalHPPanel");
+        ModalNamePanel = GameObject.Find("ModalNamePanel");
 
         // Initialize the main Views Buttons.
         StatsButton = GameObject.Find("StatsButton").GetComponent<Button>();
-        SavesButton = GameObject.Find("SavesButton").GetComponent<Button>();
+        ResistancesButton = GameObject.Find("ResistancesButton").GetComponent<Button>();
+        ACButton = GameObject.Find("ACButton").GetComponent<Button>();
+        HPButton = GameObject.Find("HPButton").GetComponent<Button>();
+        NameButton = GameObject.Find("NameButton").GetComponent<Button>();
 
         //Add the on click event to the Buttons
         //StatsButton.onClick.AddListener(ModalPanelBackdropShow);
