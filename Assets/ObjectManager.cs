1dcf8e510543a974adc94195d423f06c48038be2
petpetshoy <kenithjones@hotmail.ca>
2020-03-26 17:34:23 -0700

    update to object manager
    added in more of the variables into the script.

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ObjectManager : MonoBehaviour
 {
-
+// modal windows
     public GameObject ModalPanelBackdrop;
     public GameObject ModalStatsPanel;
     public GameObject ModalACPanel;
@@ -13,11 +13,100 @@ public class ObjectManager : MonoBehaviour
     public GameObject ModalHPPanel;
     public GameObject ModalNamePanel;
 
+
+
+
+//buttons for panel 1 and their views
     public Button StatsButton;
+    
+    public GameObject strTextView;
+    public GameObject strTextValue;
+    public GameObject strTextMod;
+    public GameObject dexTextView;
+    public GameObject dexTextValue;
+    public GameObject dexTextMod;
+    public GameObject conTextView;
+    public GameObject conTextValue;
+    public GameObject conTextMod;
+    public GameObject intTextView;
+    public GameObject intTextValue;
+    public GameObject intTextMod;
+    public GameObject wisTextView;
+    public GameObject wisTextValue;
+    public GameObject wisTextMod;
+    public GameObject chaTextView;
+    public GameObject chaTextValue;
+    public GameObject chaTextMod;
+
+
     public Button ResistancesButton;
+    
+    public GameObject drTextView;
+    public GameObject drTextValue;
+    public GameObject acidResistanceTextView;
+    public GameObject acidResistanceTextValue;
+    public GameObject coldResistanceTextView;
+    public GameObject coldResistanceTextValue;
+    public GameObject electricityResistanceTextView;
+    public GameObject electricityResistanceTextValue;
+    public GameObject fireResistanceTextView;
+    public GameObject fireResistanceTextValue;
+    public GameObject sonicResistanceTextView;
+    public GameObject sonicResistanceTextValue;
+
+
+
     public Button ACButton;
+
+    public GameObject kacLabel;
+    public GameObject kacValueLabel;
+    public GameObject eacLabel;
+    public GameObject eacValueLabel;
+    public GameObject cmdLabel;
+    public GameObject cmdValueLabel;
+    public GameObject fortTextView;
+    public GameObject fortTextValue;
+    public GameObject refTextView;
+    public GameObject refTextValue;
+    public GameObject willTextView;
+    public GameObject willTextValue;
+
+
+
     public Button HPButton;
+
+    public GameObject initValueView;
+    public GameObject initLabel;
+    public GameObject speedLabel;
+    public GameObject speedValueView;
+    public GameObject staminaLabel;
+    public GameObject hpLabel;
+    public GameObject resolveLabel;
+    public GameObject staminaValueLabel;
+    public GameObject hpValueLabel;
+    public GameObject resolveValueLabel;
+
+
+
     public Button NameButton;
+
+    public GameObject nameTextView;
+    public GameObject levelTextView;
+    public GameObject classTextView;
+
+
+
+
+
+
+    // panel 2
+
+    // panel 3
+
+    // panel 4
+
+    // panel 5
+
 
     // Start is called before the first frame update
     void Start()
